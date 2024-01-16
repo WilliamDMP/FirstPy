a986e541066304b88de9d2b53f27e170af19a68d
WilliamDMP <wdmartinez89@gmail.com>
2024-01-15 20:14:10 -0500

    contador

--- PythonBasico/HelloWorld.py
+++ PythonBasico/HelloWorld.py
@@ -5,3 +5,10 @@ edad = 34
 studying = True
 
 print(nombre, edad, studying)
+print(type(nombre), type(edad), print(studying))
+
+#utilizando contadores
+contador = 0
+contador += 1
+
+print(contador)
